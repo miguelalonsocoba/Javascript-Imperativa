6b1f1822d6a77690d164be548a57f02f695d25f5
miguelalonsocoba <miguetheking53@gmail.com>
2022-09-02 07:39:18 -0500

    Sexto ejercicio

---
+++ 0-fraction/5-divide/app.js
@@ -0,0 +1,19 @@
+const { Console } = require("console-mpds");
+
+const console = new Console();
+
+const MESSAGE_NUMERATOR = "Introduce el numerador de la fracción: ";
+const MESSAGE_DENOMINATOR = "Introduce el denominador de la fracción";
+
+console.writeln("Primera fracción:");
+const firstNumerator = console.readNumber(MESSAGE_NUMERATOR);
+const firstDenominator = console.readNumber(MESSAGE_DENOMINATOR);
+console.writeln("Segunda fracción");
+const secondNumerator = console.readNumber(MESSAGE_NUMERATOR);
+const secondDenominator = console.readNumber(MESSAGE_DENOMINATOR);
+
+const newNumerator = firstNumerator * secondDenominator;
+const newDenominator = firstDenominator * secondNumerator;
+
+console.writeln(`La división de la fracción ${firstNumerator}/${firstDenominator} y la fracción \
+${secondNumerator}/${secondDenominator} es la fracción ${newNumerator}/${newDenominator}`);
