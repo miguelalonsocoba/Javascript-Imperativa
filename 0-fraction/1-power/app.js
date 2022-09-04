41ace289662f186719f6c285ee50001fd413f024
miguelalonsocoba <miguetheking53@gmail.com>
2022-09-03 23:13:18 -0500

    Correción de ejercicio

--- 0-fraction/1-power/app.js
+++ 0-fraction/1-power/app.js
@@ -1,9 +1,9 @@
-const { Console } = require("console-mpds");
+const { Console } = require(`console-mpds`);
 
 const console = new Console();
 
-const numerator = console.readNumber("Introduce el numerador de la fraccion: ");
-const denominator = console.readNumber("Introduce el denominador de la fraccion: ");
-const exponential = console.readNumber("Introduce un exponente: ");
+const numerator = console.readNumber(`Introduce el numerador de la fraccion: `);
+const denominator = console.readNumber(`Introduce el denominador de la fraccion: `);
+const exponential = console.readNumber(`Introduce un exponente: `);
 
 console.writeln(`La fraccion ${numerator}/${denominator} elevado a ${exponential} es la fraccin ${numerator ** exponential}/${denominator ** exponential}`);
